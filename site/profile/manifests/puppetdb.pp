e6d81d0b1d91e59f66f0c2f4f3e1e299d9312b10
C.H <6haker@informatik.uni-hamburg.de>
2020-09-22 16:11:59 +0200

    Create puppetdb.pp

---
+++ site/profile/manifests/puppetdb.pp
@@ -0,0 +1,8 @@
+class profile::puppetdb {
+  node <hostname> {
+    # Configure puppetdb and its underlying database
+    class { 'puppetdb': }
+    # Configure the Puppet master to use puppetdb
+    class { 'puppetdb::master::config': }
+  }
+}
